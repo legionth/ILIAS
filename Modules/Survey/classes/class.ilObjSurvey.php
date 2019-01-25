8df96733bab917023356fc45a483a467d6f3b188
mjansen <mjansen@databay.de>
2019-01-25 10:01:28 +0100

    Mail: Got rit of 'SOAP' (internally and in consumer code) / Used correct user id for automatically initiated notificatons)

--- Modules/Survey/classes/class.ilObjSurvey.php
+++ Modules/Survey/classes/class.ilObjSurvey.php
@@ -6292,7 +6292,6 @@ class ilObjSurvey extends ilObject
 
 		// #10044
 		$mail = new ilMail(ANONYMOUS_USER_ID);
-		$mail->enableSOAP(false); // #10410
 		$mail->sendMail(ilObjUser::_lookupLogin($a_user_id),
 			null,
 			null,
--- Services/Init/classes/class.ilStartUpGUI.php
+++ Services/Init/classes/class.ilStartUpGUI.php
@@ -2204,7 +2204,6 @@ class ilStartUpGUI
 	
 				$body .= ($lng->txt("reg_mail_body_text3")."\n\r");
 				$body .= $oUser->getProfileAsString($lng);
-				$mail_obj->enableSoap(false);
 				$mail_obj->appendInstallationSignature(true);
 				$mail_obj->sendMail($oUser->getEmail(), '', '',
 					$subject,
